7b8e7681f4415bdc77c1124a1d162ff04058f521
Cumstein <kamyabaghaz@gmail.com>
2025-05-15 22:53:58 +0330

    ads responsive change

--- src/app/@ads/products/page.tsx
+++ src/app/@ads/products/page.tsx
@@ -12,7 +12,7 @@ const Ads = () => {
   return (
     <Card className="w-3/4 flex flex-col-reverse md:flex-row justify-between items-center overflow-hidden p-2">
       <div>
-        <CardHeader>
+        <CardHeader className='flex justify-between items-center'>
           <CardTitle>On Sale products</CardTitle>
           <CardDescription>
             Buy outdated products with 50% discount
